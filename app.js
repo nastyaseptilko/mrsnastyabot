f4f5f71de642015adaea64bdabc985016497842b
a.septilko <a.septilko@pixelplex.io>
2022-08-25 19:34:21 +0300

    Fix err

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const axios = require('axios');
 
 const { PORT, TELEGRAM_TOKEN, SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
-// const TELEGRAM_FILE_API = `https://api.telegram.org/file/bot${TELEGRAM_TOKEN}`;
 
 const setupWebhook = async () => {
     try {
@@ -32,12 +31,10 @@ app.post('/', (req, res) => {
 	const { message } = req.body
 	const photo = message.photo;
 
-	console.log(photo, 'photo')
 	if(!message || !photo) {
 		return res.end();
 	}
-	if(photo.lenght){
-		console.log('fff', photo[0].file_id)
+	if(photo.length){
 		axios
 		.post(
 			`${TELEGRAM_API}/sendPhoto`,
@@ -50,7 +47,7 @@ app.post('/', (req, res) => {
 			res.end('ok');
 		})
 		.catch((err) => {
-			res.end('When you send an error has occurred:' + err);
+			res.end('Photo does not send:' + err);
 		})
 	}
 	axios
