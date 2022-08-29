71c6536b31a485c53c618f991c5e397645607f48
a.septilko <a.septilko@pixelplex.io>
2022-08-29 20:42:46 +0300

    Add random cats

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const path = require('path');
 const { sendMessage } = require('./helpers/send-message');
 const { sendPhoto } = require('./helpers/send-photo');
+const { sendAnimation } = require('./helpers/send-animation');
 
-const { PORT, TELEGRAM_TOKEN, SERVER_URL } = process.env;
+const { PORT, TELEGRAM_TOKEN, SERVER_URL, GIPHY_API_KEY } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+const GIPHY_API = `https://api.giphy.com/v1`
+const TELEGRAM_MENU = {
+	cats: '/cats'
+};
 
 const setupWebhook = async () => {
     try {
 		console.log(1);
         await axios.get(`${TELEGRAM_API}/setWebhook?url=${SERVER_URL}&drop_pending_updates=true`);
     } catch (error) {
-		console.log(2);
-
         return error;
     }
 }
@@ -27,12 +31,13 @@ app.use(
 		extended: true,
 	})
 );
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/name', (req, res) => {
     res.send('<h1> Telegram server </h1>');
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 	const { message } = req.body
 	const photo = message.photo;
 
@@ -41,6 +46,14 @@ app.post('/', (req, res) => {
 	}
 	if(photo && photo.length){
 		return sendPhoto(res, TELEGRAM_API, message);
+	}
+	if(message.text === TELEGRAM_MENU.cats) {
+		const tag = 'cat';
+		const result = await axios.get(`${GIPHY_API}/gifs/random?api_key=${GIPHY_API_KEY}&tag=${tag}`);
+		if(!result) {
+			return res.end('Random gif not received');
+		}
+		return sendAnimation(res, TELEGRAM_API, message.chat.id, result.data.data.images.original.url);
 	}
 	return sendMessage(res, TELEGRAM_API, message);
 });
