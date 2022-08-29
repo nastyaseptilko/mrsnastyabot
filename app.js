cc9fc6dd1263c5182b50464c23bd022490d825ae
a.septilko <a.septilko@pixelplex.io>
2022-08-29 17:00:10 +0300

    Fix err

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const axios = require('axios').default;
 
 const { PORT, TELEGRAM_TOKEN, SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 
 const setupWebhook = async () => {
     try {
+		console.log(1);
         await axios.get(`${TELEGRAM_API}/setWebhook?url=${SERVER_URL}&drop_pending_updates=true`);
     } catch (error) {
+		console.log(2);
+
         return error;
     }
 }
@@ -27,7 +30,7 @@ app.get('/name', (req, res) => {
     res.send('<h1> Telegram server </h1>');
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
 	const { message } = req.body
 	const photo = message.photo;
 
@@ -35,8 +38,7 @@ app.post('/', (req, res) => {
 		return res.end();
 	}
 	if(photo && photo.length){
-		return axios
-		.post(
+		return axios.post(
 			`${TELEGRAM_API}/sendPhoto`,
 			{
 				chat_id: message.chat.id,
@@ -51,20 +53,20 @@ app.post('/', (req, res) => {
 			res.end('Photo does not send:' + err);
 		})
 	}
-	axios
-		.post(
+	console.log(message, 'message');
+	await axios.post(
 			`${TELEGRAM_API}/sendMessage`,
 			{
 				chat_id: message.chat.id,
 				text: message.text,
 			}
-		)
-		.then(() => {
+		).then(() => {
+			console.log(222);
 			res.end('ok');
 		})
 		.catch((err) => {
 			res.end('When you send an error has occurred:' + err);
-		})
+		})		
 });
 
 app.listen(PORT, async () => {
